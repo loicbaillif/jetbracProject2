37b2e77962b7dc1ea9a65b9f3bcd2f448e74debb
LoicBaillif <larmedeciel@gmail.com>
2022-01-07 23:08:31 +0100

    Update HelloWorld.java

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -1,10 +1,23 @@
 package project1;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world!");
+		char letter = 'A';
+		int i = 0;
+		char[] alphabet = new char[26];
+		while (letter <= 'Z') {
+			alphabet[i] = letter;
+			i++;
+			letter++;
+		}
+		
+		System.out.println(alphabet);
+		
 	}
 
 }
