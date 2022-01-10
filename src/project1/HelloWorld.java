0117e0b1e87fc31c8589d5d5d3f78eaf7a1c91c5
loicbaillif <larmedeciel@gmail.com>
2022-01-10 22:26:37 +0100

    Update HelloWorld.java

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -8,19 +8,40 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world!");
 		Scanner scanner = new Scanner(System.in);
-		int nbElts = scanner.nextInt();
-        int[] a = new int[nbElts];
-        for (short i = 1; i <= nbElts; i++) {
-            if (i != nbElts) {
-                a[i] = scanner.nextInt();
-            } else {
-                a[0] = scanner.nextInt();
-            }
-        }
-        for (int elt: a) {
-        	System.out.format("%d ", elt);
-        }
+
+		int[] testList = {10, 3, 5, 21, 2, 4, 9};
+		
+		int[] testList2 = sortInt(testList);
+		
+		for (int elt : testList2) {
+			System.out.format("%d%n", elt);
+		}
+		
+		System.out.println("***** END OF PROGRAM *****");
 		scanner.close();
+	}
+	
+	public static int[] sortInt(int[] listInt) {
+		/**
+		 * sortInt : Will sort a list of integers in ascending order;
+		 * Probably not the most efficient method to sort a list of integers
+		 * but I wrote this little algorithm on my own.
+		 */
+		int pivot = 0;
+		int nbSwitches = 0;
+		
+		do {
+			nbSwitches = 0;
+			for (int i = 0; i < listInt.length - 1; i++) {
+				if (listInt[i+1] < listInt[i]) {
+					pivot = listInt[i];
+					listInt[i] = listInt[i+1];
+					listInt[i+1] = pivot;
+					nbSwitches++;					
+				}
+			}
+		} while (nbSwitches != 0);
+		return listInt;
 	}
 
 }
