32966ef5a81420a9b2a386afa1a3c37124a3f0c4
loicbaillif <larmedeciel@gmail.com>
2022-01-13 20:18:53 +0100

    New Exercise
    Index of first max

---
+++ src/iteratingOverArrays/IndexFirstMax.java
@@ -0,0 +1,24 @@
+package iteratingOverArrays;
+
+import java.util.Scanner;
+
+public class IndexFirstMax {
+
+	public static void main(String[] args) {
+		/* ****** EXERCISE : Index of first max ******
+		 * Write a program that reads an array of ints and finds the index of 
+		 * the first maximum in that array. 
+		 * 
+		 * --> Input data format 
+		 * 		The first line contains the number of elements in the array. 
+		 * 		The second line contains the array elements separated by spaces. 
+		 * 		An array always has at least one element. 
+		 * 
+		 * --> Output data format 
+		 * 		Only a single integer value: the index of the first maximum.
+		 */
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
