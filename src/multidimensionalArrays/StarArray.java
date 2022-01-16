74d460b1017feb1063c179c7f856e354d925e362
loicbaillif <larmedeciel@gmail.com>
2022-01-16 12:02:16 +0100

    New exercise
    Star Array

--- src/multidimensionalArrays/StarArray.java
+++ src/multidimensionalArrays/StarArray.java
@@ -26,8 +26,17 @@ public class StarArray {
 		
 		Scanner scanner = new Scanner(System.in);
 		byte nInput = scanner.nextByte(); // n doesn't exceed 15 ==> byte;
-		
-		
+		byte middle = (byte) (nInput / 2);
+		for (int i = 0; i < nInput; i++) {
+			for (int j = 0; j < nInput; j++) {
+				if (i == middle || j == i || j == nInput - i - 1 || j == middle) {
+					System.out.print("* ");
+				} else {
+					System.out.print(". ");
+				}
+			}
+			System.out.format("%n");
+		}
 		
 		scanner.close();
 		System.out.println("****** END OF PROGRAM ******");
