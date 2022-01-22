a3810c9a54736167dd6d49545535eb4118cf0104
LoicBaillif <larmedeciel@gmail.com>
2022-01-22 18:22:51 +0100

    Project completed

--- src/tictactoe/CheckField.java
+++ src/tictactoe/CheckField.java
@@ -48,53 +48,49 @@ public class CheckField {
 				System.out.println("Invalid game grid.");
 			}
 			System.out.print("Enter cells: ");
-			input = scanner.nextLine();
+			// Conversion to upper case to treat only X and O 
+			input = scanner.nextLine().toUpperCase();
 		} while (!validInput(input));
 
 		int[] countElts = new int[2];
 		countElts = countMoves(input);
 		int nbO = countElts[0];
 		int nbX = countElts[1];
-		System.out.format("%d rounds: %d moves from %c - %d moves from %c.%n", 
-				nbO + nbX, nbO, player0, nbX, player1);
 		
-		char temp;
 		char[][] stage = new char[size][size];
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				temp = input.charAt(size * i + j);
-				if (temp == 'x' || temp == 'o') {
-					temp = Character.toUpperCase(temp);
-				}
-				stage[i][j] = temp;
+				stage[i][j] = input.charAt(size * i + j);
 			}
 		}
 		displayGrid(stage);
+		statusGame(stage, nbO, nbX);
 
 		scanner.close();
 	}
 
 
-
-
 	public static int[] countMoves(String stage) {
+		// This method  returns  an array made of 2 elements:
+		// 1st is nb of moves from player 0 (using O)
+		// 2nd is nb of moves from player 1 (using X)
 		// Checked and validated.
 		int nbO = 0;
 		int nbX = 0;
-		for (int i = 0; i < size*size; i++) {
+		for (int i = 0; i < maxRounds; i++) {
 			if (stage.charAt(i) == player0) {
 				nbO++;
 			} else if (stage.charAt(i) == player1) {
 				nbX++;
 			}
 		}
-
 		int[] result = {nbO, nbX}; // Alphabetical Order
 		return result;
 	}
 
 
 	public static void displayGrid(char[][] stage) {
+		// Displays the grid in the console, within a frame.
 		// Checked and validated.
 		System.out.println("---------");
 		for (int i = 0; i < size; i++) {
@@ -108,7 +104,56 @@ public class CheckField {
 	}
 
 
+	public static void statusGame(char[][] stage, int nbO, int nbX) {
+		// 0 = player0 (O) wins - 1 = player1 (X) wins - 2 = game not finished
+		// 3 = impossible - 4 = draw
+		// Checked and validated
+		byte result;
+		int nbMoves = nbO + nbX;
+		if (Math.abs(nbO - nbX) > 1) {
+			// CAUTION : Always remember to check consistency first.
+			// Too big difference between number of O and X : impossible
+			result = 3;
+		} else if (nbO < size && nbX < size) {
+			// Less than 3 O and 3 X ? can't be a winner, game not finished
+			result = 2;
+		} else if (wonGame(stage, player0) && wonGame(stage, player1)) {
+			// Two winners? Sorry, not possible
+			result = 3;
+		} else if (wonGame(stage, player0)) {
+			result = 0;
+		} else if (wonGame(stage, player1)) {
+			result = 1;
+		} else if (nbMoves < maxRounds) {
+			// No winner, but there are still empty cells
+			result = 2;
+		} else {
+			// no winner, no empty cells, no impossible status
+			result = 4;
+		}
+		switch (result) {
+		case 0:
+			System.out.println("O wins");
+			break;
+		case 1:
+			System.out.println("X wins");
+			break;
+		case 2:
+			System.out.println("Game not finished");
+			break;
+		case 3:
+			System.out.println("Impossible");
+			break;
+		case 4:
+			System.out.println("Draw");
+		}
+	}
+
+
 	public static boolean validInput(String input) {
+		// Ensure that the input string is valid, id est :
+		// --> Made of 9 characters.
+		// --> Consists of O or X or _ and nothing else.
 		// Checked and validated
 		char[] inputC = input.toCharArray();
 		int nbMoves = inputC.length;
@@ -117,8 +162,7 @@ public class CheckField {
 			result = false;
 		} else {
 			for (char elt : inputC) {
-				System.out.format("%c - maxRounds = %d %n", elt, maxRounds);
-				if (elt == 'o' || elt == 'O' || elt == 'x' || elt == 'X' || elt == '_') {
+				if (elt == 'O' || elt == 'X' || elt == '_') {
 					continue;
 				} else {
 					result = false;
@@ -130,17 +174,50 @@ public class CheckField {
 	}
 
 
-	public static byte wonGame(char[][] stage, int nbO, int nbX) {
-		// Writing in progress
-		// 0 = noWin - 1 = player0 (O) wins - 2 = player1 (X) wins
-		// 3 = both win (impossible)
-		byte result = 0; // no winner
-		if (nbO < 2 && nbX < 2) {
-			result = 0;
-		} 
-		
+	public static boolean wonGame(char[][] stage, char player) {
+		// Returns true if player won the game, id est 1 of the 8:
+		// 3 in a row in a line OR
+		// 3 in a row in a column OR
+		// 3 in a row in a diagonal 
+		// Checked and validated
+		boolean result = false;
+		for (int i = 0; i < size; i++) {
+			// Check lines
+			if ((stage[i][0] == player) 
+					&& (stage[i][1] == player) 
+					&& (stage[i][2] == player)) {
+				result = true;
+				break; // No need to check other lines
+				// System.out.format("%c won on line %d%n", player, i); debug
+			}
+		}
+		for (int j = 0; j < size; j++) {
+			// Check columns
+			if ((stage[0][j] == player)  
+					&& (stage[1][j] == player)  
+					&& (stage[2][j] == player)) {
+				result = true;
+				break; // No need to check other lines
+				// System.out.format("%c won on column %d%n", player, j); debug
+			}
+		}
+		if ((stage[0][0] == player)  
+				&& (stage[1][1] == player)  
+				&& (stage[2][2] == player)) {
+			// Check one diagonal
+			result = true;
+			// System.out.println("Won on descending diagonal"); debug
+		} else if ((stage[0][2] == player) 
+				&& (stage[1][1] == player) 
+				&& (stage[2][0] == player)) {
+			// check the other diagonal
+			result = true;
+			// System.out.println("Won on ascending diagonal"); debug
+		}
+
 		return result;
 	}
+
 
 }
 
