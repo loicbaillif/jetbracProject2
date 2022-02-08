0d8ad664a14853bf6bf62a8ab8f31c126f86b659
loicbaillif <larmedeciel@gmail.com>
2022-02-08 22:51:41 +0100

    Correction in variable

--- src/nullPointerException/correctCode.java
+++ src/nullPointerException/correctCode.java
@@ -8,7 +8,7 @@ package nullPointerException;
 public class correctCode {
 
 	public static void main(String[] args) {
-		System.out.println(name == null ? 0 : name.length());
+		System.out.println(args == null ? 0 : args.length);
 
 	}
 
