a47792d9ba4317bb1aee3844e3749aae95e29a16
loicbaillif <larmedeciel@gmail.com>
2022-02-08 21:31:47 +0100

    Exercise
    NPE : Return Empty String

---
+++ src/nullPointerException/ReturnEmptyString.java
@@ -0,0 +1,40 @@
+package nullPointerException;
+
+import java.util.Scanner;
+import java.util.Locale;
+import java.util.Objects;
+
+/* ****** EXERCISE : Return Empty String ******
+ * 
+ * Take a look at the method that takes a string, converts all characters of 
+ * the string to upper case, and then returns the resulting string.
+ * 
+ * But there is a problem. Sometimes an input string is null. As you already 
+ * know, in this case, the method throws a NullPointerException.
+ * 
+ * Your task is to fix the method in order to avoid an NPE.
+ * 
+ * If the input string is null, the method should return the empty string "".
+ */
+
+public class ReturnEmptyString {
+
+	// Fix this method:
+	public static String toUpperCase(String str) {
+		if (Objects.equals(str, null)) {
+			return "";
+		} else {
+			return str.toUpperCase(Locale.ENGLISH);
+		}
+	}
+
+
+	// Do not change the code below
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		line = "none".equalsIgnoreCase(line) ? null : line;
+		System.out.println(toUpperCase(line));
+	}
+
+}
