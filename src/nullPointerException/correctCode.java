7ea98161947e1f2be413a2f0146b8009167d6a16
loicbaillif <larmedeciel@gmail.com>
2022-02-08 22:44:46 +0100

    Exercise

---
+++ src/nullPointerException/correctCode.java
@@ -0,0 +1,15 @@
+package nullPointerException;
+
+/* ****** EXERCISE : Unknown input ******
+ * 
+ * Correct the code so it doesn't throw an exception.
+ */
+
+public class correctCode {
+
+	public static void main(String[] args) {
+		System.out.println(name == null ? 0 : name.length());
+
+	}
+
+}
