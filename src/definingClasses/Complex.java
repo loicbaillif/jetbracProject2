058fb2741ae0596a95dbabf2a36964cf79e26bbb
loicbaillif <larmedeciel@gmail.com>
2022-02-18 17:35:23 +0100

    Exercise

---
+++ src/definingClasses/Complex.java
@@ -0,0 +1,15 @@
+package definingClasses;
+
+/* ****** EXERCISE : Complex ******
+ * 
+ * You are creating a calculator that manipulates complex numbers. 
+ * Write a class named Complex. It must have two double fields 
+ * named real and imaginary.
+ */
+
+public class Complex {
+	
+	double imaginary;
+	double real;
+	
+}
