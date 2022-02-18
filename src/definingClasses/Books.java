a6fbc0676b18f787097c435710d84960f31f0133
loicbaillif <larmedeciel@gmail.com>
2022-02-18 17:39:26 +0100

    Exercise

---
+++ src/definingClasses/Books.java
@@ -0,0 +1,17 @@
+package definingClasses;
+
+/* ****** EXERCISE : Books ******
+ * 
+ * Write a class named Book. It must have three fields: 
+ * a string field title, 
+ * an int field yearOfPublishing, 
+ * and a boolean field isAvailable.
+ */
+
+public class Books {
+
+	String title;
+	int yearOfPublishing;
+	boolean isAvailable;
+
+}
