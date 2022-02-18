e32e67fd383a908a93ecf71a2b16ce22ba5a4ad1
loicbaillif <larmedeciel@gmail.com>
2022-02-18 17:50:00 +0100

    Exercise

---
+++ src/definingClasses/Account.java
@@ -0,0 +1,17 @@
+package definingClasses;
+
+/* ****** EXERCISE : Accounts ******
+ * 
+ * Define a class named Account. It must have three fields: 
+ * a long field balance, 
+ * a string field ownerName, 
+ * and a boolean flag locked.
+ */
+
+public class Account {
+
+	boolean locked;
+	long balance;
+	String ownerName;
+	
+}
