5d581197fbce0b8c4a761fc10991881c09f02235
loicbaillif <larmedeciel@gmail.com>
2022-02-18 17:44:30 +0100

    Exercise

---
+++ src/definingClasses/Time.java
@@ -0,0 +1,17 @@
+package definingClasses;
+
+/* ****** EXERCISE : Representing time as a class ******
+ * 
+ * Define a class named Time. It must have three int fields: 
+ * hours, minutes and seconds.
+ * 
+ * Note: do not make fields private.
+ */
+
+class Time {
+
+	int hours;
+	int minutes;
+	int seconds;
+
+}
