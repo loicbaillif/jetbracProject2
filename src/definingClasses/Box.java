780637232def15d4750da3817b89d639dda43e28
loicbaillif <larmedeciel@gmail.com>
2022-02-18 17:22:33 +0100

    Classes - Exercise
    Inner Boxes

---
+++ src/definingClasses/Box.java
@@ -0,0 +1,24 @@
+package definingClasses;
+
+/* ****** EXERCISE : Inner Boxes ******
+ * 
+ * Define a class named Box. It must have four fields:
+ * 
+ * - length, height and width of the double type
+ * - a field named innerBox of the same type as the defined class
+ * Do not make the fields private.
+ */
+
+public class Box {
+	
+	double length;
+	double height;
+	double width;
+	Box innerBox;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
