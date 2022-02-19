07930bf7e47841fd191eecc38602487825cac67d
loicbaillif <larmedeciel@gmail.com>
2022-02-19 16:15:59 +0100

    Exercise
    Account

---
+++ src/constructor/Account.java
@@ -0,0 +1,25 @@
+package constructor;
+
+/* ****** EXERCISE : Account ******
+ * 
+ * Write a class named Account. It must have three fields: 
+ * a long field balance, 
+ * a string field ownerName, 
+ * and a boolean flag locked, 
+ * and a constructor with three parameters to initialize these fields. The
+ * order of parameters in the constructor must be the same as presented above.
+ */
+
+class Account {
+	
+	long balance;
+	String ownerName;
+	boolean locked;
+	
+	public Account(long balance, String ownerName, boolean locked) {
+		this.balance = balance;
+		this.ownerName = ownerName;
+		this.locked = locked;
+		System.out.println("Welcome to my bank");
+	}
+}
