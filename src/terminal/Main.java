b309b7ab336b2e3dea4be1a84458dff864bcc400
LoicBaillif <larmedeciel@gmail.com>
2022-02-19 11:54:04 +0100

    Java in Terminal

---
+++ src/terminal/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How old?");
+        int age = scanner.nextInt();
+        if (age >= 18) {
+            System.out.println("You can drive a car!");
+        } else {
+            System.out.println("You can ask your parents for money");
+        }
+    }
+}
