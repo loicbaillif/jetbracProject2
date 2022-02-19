a98b071219c09a6fafeaf58080eaab889719add2
loicbaillif <larmedeciel@gmail.com>
2022-02-19 16:08:59 +0100

    Exercise
    User

---
+++ src/constructor/User.java
@@ -0,0 +1,28 @@
+package constructor;
+
+/* ****** EXERCISE : User ******
+ * 
+ * Write a class named User. It must have three string fields: 
+ * login, firstName, and lastName 
+ * and a constructor with three parameters to initialize these fields. The 
+ * order of parameters in the constructor must be the same as presented above.
+ * 
+ * Do not make the fields and the constructor private. 
+ * Do not make the User class public.
+ * 
+ */
+
+
+class User {
+
+	String login;
+	String firstName;
+	String lastName;
+	
+	public User(String login, String firstName, String lastName) {
+		this.login = login;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+}
