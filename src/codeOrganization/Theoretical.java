b26a46fbb7a5cc3cddb01a68f4dc21958f1d8b24
loicbaillif <larmedeciel@gmail.com>
2022-03-13 21:32:51 +0100

    New Chapter

---
+++ src/codeOrganization/Theoretical.java
@@ -0,0 +1,10 @@
+package codeOrganization;
+
+public class Theoretical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Theoretical.class.getName());
+	}
+
+}
