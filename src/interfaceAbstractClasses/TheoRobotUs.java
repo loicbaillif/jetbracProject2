e7300a8cf5ca2a8ee57e1f583ab877c823164d9a
loicbaillif <larmedeciel@gmail.com>
2022-03-23 21:47:22 +0100

    Update TheoRobotUs.java

--- src/interfaceAbstractClasses/TheoRobotUs.java
+++ src/interfaceAbstractClasses/TheoRobotUs.java
@@ -6,15 +6,15 @@ public class TheoRobotUs implements TheoryInterface {
 		int result = userInput + userInput;
 		System.out.println("Oh yeah, the Yankee way!");
 		System.out.printf("You want me to multiply %d by 5 dude?", userInput);
-		System.out.println("I can do it in 5 steps!");
-		System.out.printf("\t1) %d + %d = %d", userInput, userInput, result);
-		System.out.printf("\t2) %d + %d = %d", result, userInput, 
+		System.out.println("I can do it in 4 steps!");
+		System.out.printf("\t1) %d + %d = %d%n", userInput, userInput, result);
+		System.out.printf("\t2) %d + %d = %d%n", result, userInput, 
 				result + userInput);
 		result += userInput;
-		System.out.printf("\t3) %d + %d = %d", result, userInput, 
+		System.out.printf("\t3) %d + %d = %d%n", result, userInput, 
 				result + userInput);
 		result += userInput;
-		System.out.printf("\t4) %d + %d = %d", result, userInput, 
+		System.out.printf("\t4) %d + %d = %d%n", result, userInput, 
 				result + userInput);
 		result += userInput;
 		
