4cc3e9d10c10c18d56c8d27f7566c2dc8b68bb47
loicbaillif <larmedeciel@gmail.com>
2022-03-23 21:07:55 +0100

    Theory

--- src/interfaceAbstractClasses/Theory.java
+++ src/interfaceAbstractClasses/Theory.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class Theory implements TheoryInterface {
 	
 	public int timesFive(int userInput) {
-		System.out.println("Multiply by 5? Easy peasy.");
+		System.out.printf("Multiply %d by 5? Easy peasy.%n", userInput);
 		System.out.println("I add a 0 at the end ... which gives ...");
 		System.out.printf("\t%d%n", userInput * 10);
 		System.out.println("Then I divide by 2! which gives ...");
@@ -14,8 +14,21 @@ class Theory implements TheoryInterface {
 	}
 	
 	public boolean isEven(int userInput) {
-		System.out.println("Class Theory: instanceMethod2 ...");
-		return false;
+		boolean result = false;
+		int lastDigit = userInput % 10;
+		System.out.println("Check if %d is even? even easier ...");
+		System.out.printf("First, get %d last digit: %d", 
+				userInput, lastDigit);
+		System.out.printf("Then if it equals 0, 2, 4, 6 or 8 it means"
+				+ "%d is even", userInput);
+		if (lastDigit == 0
+				|| lastDigit == 2
+				|| lastDigit == 4 
+				|| lastDigit == 6 
+				|| lastDigit == 8) {
+			result = true;
+		}
+		return result;
 	}
 	
 	TheoryInterface instance = new Theory();
@@ -26,6 +39,7 @@ class Theory implements TheoryInterface {
 		Theory t1 = new Theory();
 		int userInput = 1;
 		int result;
+		boolean isEven;
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a natural int");
 		if (scanner.hasNextInt() && scanner.nextInt() > 0) {
@@ -35,6 +49,8 @@ class Theory implements TheoryInterface {
 		}
 		result = t1.timesFive(userInput);
 		System.out.println(result);
+		isEven = t1.isEven(userInput);
+		System.out.printf("%d is even? %b", userInput, isEven);
 		
 	}
 	
