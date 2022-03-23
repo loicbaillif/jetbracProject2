62960b2487d6001541fed0820028273507d946a5
loicbaillif <larmedeciel@gmail.com>
2022-03-23 21:39:22 +0100

    Second implementation of interface
    RobotUs

---
+++ src/interfaceAbstractClasses/TheoRobotUs.java
@@ -0,0 +1,27 @@
+package interfaceAbstractClasses;
+
+public class TheoRobotUs implements TheoryInterface {
+	
+	public int timesFive(int userInput) {
+		int result = userInput + userInput;
+		System.out.println("Oh yeah, the Yankee way!");
+		System.out.printf("You want me to multiply %d by 5 dude?", userInput);
+		System.out.println("I can do it in 5 steps!");
+		System.out.printf("\t1) %d + %d = %d", userInput, userInput, result);
+		System.out.printf("\t2) %d + %d = %d", result, userInput, 
+				result + userInput);
+		result += userInput;
+		System.out.printf("\t3) %d + %d = %d", result, userInput, 
+				result + userInput);
+		result += userInput;
+		System.out.printf("\t4) %d + %d = %d", result, userInput, 
+				result + userInput);
+		result += userInput;
+		
+		return result;
+	}
+	
+	public boolean isEven(int userInput) {
+		return false;
+	}
+}
