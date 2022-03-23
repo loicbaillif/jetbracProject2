a0b141aeb83cb7811e219a2eebac65f2ce63e722
loicbaillif <larmedeciel@gmail.com>
2022-03-23 21:47:26 +0100

    Update TheoryExample.java

--- src/interfaceAbstractClasses/TheoryExample.java
+++ src/interfaceAbstractClasses/TheoryExample.java
@@ -6,6 +6,7 @@ public class TheoryExample {
 
 	public static void main(String[] args) {
 		TheoRobotEu tr1 = new TheoRobotEu();
+		TheoRobotUs tr2 = new TheoRobotUs();
 		int userInput = 1;
 		int result;
 		boolean isEven;
@@ -20,10 +21,17 @@ public class TheoryExample {
 		} else {
 			scanner.next();
 		}
+		System.out.println("\n\t****** FIRST EXERCISE ******\n");
 		result = tr1.timesFive(userInput);
 		System.out.printf("******%n%d*5 = %d%n", userInput, result);
+		result = tr2.timesFive(userInput);
+		System.out.printf("******%n%d*5 = %d%n", userInput, result);
+		System.out.println("\n\t****** SECOND EXERCISE ******\n");
 		isEven = tr1.isEven(userInput);
-		System.out.printf("******%n%d is even? %b", userInput, isEven);
+		System.out.printf("******%n%d is even? %b%n", userInput, isEven);
+		isEven = tr2.isEven(userInput);
+		System.out.printf("******%n%d is even? %b%n", userInput, isEven);
+		
 	}
 
 }
