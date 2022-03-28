8d73c6cd43d34c4ce49d493d85ef1467a2e64478
loicbaillif <larmedeciel@gmail.com>
2022-03-28 21:34:41 +0200

    Interface : Exercise
    Work in progress ...

--- src/interfaceAbstractClasses/CompactStrings.java
+++ src/interfaceAbstractClasses/CompactStrings.java
@@ -29,6 +29,43 @@ package interfaceAbstractClasses;
 
 import java.util.*;
 
-public class CompactStrings {
+class CompactStrings implements java.lang.CharSequence {
+
+	byte[] compactStringArray;
+
+	public CompactStrings(byte[] charSequence) {
+		this.compactStringArray = charSequence.clone();
+	}
+
+
+	@Override
+	public int length() {
+		if (!Objects.equals(compactStringArray, null)) {
+			return compactStringArray.length;
+		}
+		return 0;
+	}
+
+	@Override
+	public char charAt(int index) {
+		if (compactStringArray.length > index) {
+			return (char) compactStringArray[index];
+		}
+		return 0;
+	}
+
+	@Override
+	public CharSequence subSequence(int start, int end) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO 
+		return "";
+	}
+
 
 }
